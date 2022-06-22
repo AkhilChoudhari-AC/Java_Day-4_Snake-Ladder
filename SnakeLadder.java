ecbe87c6f851ce3b55101a61bafb5af54d725d05
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-06-22 21:22:22 +0530

    Snake and Ladder game played with single player at start position 0

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -4,5 +4,6 @@ public class SnakeLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to the Snake and Ladder Game .\nA Single player starts from the position 0(zero)" );
 
+
     }
 }
