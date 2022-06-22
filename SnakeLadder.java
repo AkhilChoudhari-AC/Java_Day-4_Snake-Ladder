af24e09e65e36004e9f3718a9dbfd130c3c9204a
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-06-22 21:19:04 +0530

    Welcome to the Snake and Ladder Game Program

---
+++ SnakeLadder.java
@@ -0,0 +1,8 @@
+package Day4SnakeLadderProgram;
+
+public class SnakeLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Snake and Ladder Game .\nA Single player starts from the position 0(zero)" );
+
+    }
+}
